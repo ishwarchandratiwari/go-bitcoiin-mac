9a36267f21d7c299477b358ab95d327ea34a008a
Masterdubs <masterdubs@pirl.io>
2019-05-01 22:17:43 +0200

    add versioning

--- params/version.go
+++ params/version.go
@@ -24,7 +24,7 @@ const (
 	VersionMajor = 1        // Major version component of the current release
 	VersionMinor = 8        // Minor version component of the current release
 	VersionPatch = 27       // Patch version component of the current release
-	VersionMeta  = "gecko" // Version metadata to append to the version string
+	VersionMeta  = "atlantis" // Version metadata to append to the version string
 
 )
 
